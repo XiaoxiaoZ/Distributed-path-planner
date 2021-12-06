33997c597bb3366f704ccbd6403fafbf708bf0da
XiaoxiaoZ <zxxzxxv@gmail.com>
2021-12-06 21:52:24 +0100

    Init elixir workspace
    Init elixir workspace

---
+++ dpp/lib/dpp.ex
@@ -0,0 +1,18 @@
+defmodule DPP do
+  @moduledoc """
+  Documentation for `DPP`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DPP.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ dpp/test/dpp_test.exs
@@ -0,0 +1,8 @@
+defmodule DPPTest do
+  use ExUnit.Case
+  doctest DPP
+
+  test "greets the world" do
+    assert DPP.hello() == :world
+  end
+end
